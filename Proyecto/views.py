fb5c49c08d24ceb9593644507395c5661caaefe7
Jairth <rodriguez6191@hotmail.com>
2022-09-03 22:54:15 -0500

    first commit

---
+++ Proyecto/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+  return render(request, 'index.html')
+
+def carta(request):
+  return render(request, 'carta.html')
+
+def contacto(request):
+  return render(request, 'contacto.html')
+
+def nosotros(request):
+  return render(request,'nosotros.html')
+
+def pedidos(request):
+  return render(request, 'pedidos.html')
